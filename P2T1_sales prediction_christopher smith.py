433c91389a61aa72051cd161e377830dab8b9c46
smith2086 <55675743+smith2086@users.noreply.github.com>
2019-09-22 21:52:57 -0500

    Add files via upload

---
+++ P2T1_sales prediction_christopher smith.py
@@ -0,0 +1,16 @@
+# Get the projected total sales.
+# 9/17/2019
+# CTI-110 P2T1 - Sales Prediction
+# SmithChristopher
+# 
+
+# Get the projected total sales.
+total_sales=float(input(';enter the projected sales: '))
+
+#calculate the profits as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print('The profit is $', profit)
+                  
+                  
